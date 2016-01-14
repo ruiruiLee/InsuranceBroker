7f9d9a6f1bcb015619faf8778aa536ec63f32280
forrest <14351834@qq.com>
2016-01-14 20:54:27 +0800

    Initial Commit

---
+++ ibroker/ibroker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ibroker
+//
+//  Created by forrestLee on 1/14/16.
+//  Copyright (c) 2016 cmkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ibroker/ibroker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ibroker
+//
+//  Created by forrestLee on 1/14/16.
+//  Copyright (c) 2016 cmkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
